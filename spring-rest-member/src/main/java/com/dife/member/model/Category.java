79f636a12554d3564d01376816e8d5abcfb59d2c
suyeon <stgood@kookmin.ac.kr>
2024-03-17 00:37:24 +0900

    feat: Member 서비스 추가 세팅 및 id수정

---
+++ spring-rest-member/src/main/java/com/dife/member/model/Category.java
@@ -0,0 +1,20 @@
+package com.dife.member.model;
+
+public enum Category {
+    ISTJ,
+    ISFJ,
+    INFJ,
+    INTJ,
+    ISTP,
+    ISFP,
+    INFP,
+    INTP,
+    ESTP,
+    ESFP,
+    ENFP,
+    ENTP,
+    ESTJ,
+    ESFJ,
+    ENFJ,
+    ENTJ
+}
