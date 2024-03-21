b7d903c785f20cdf70eaa5c9c48e47036acd48a5
Seungho <poream3387@gmail.com>
2024-03-21 15:25:16 +0900

    feat(security): 유저의 비밀번호를 암호화하자
    개요/수정사항:
    
    - 유저의 비밀번호는 그대로 저장하지 말고 암호화하여 저장하자

--- spring-rest-member/src/main/java/com/dife/member/service/MemberService.java
+++ spring-rest-member/src/main/java/com/dife/member/service/MemberService.java
@@ -5,6 +5,7 @@ import com.dife.member.repository.MemberRepository;
 import dto.RegisterRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +16,13 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public void register(RegisterRequestDto dto) {
         Member member = modelMapper.map(dto, Member.class);
+
+        String encodedPassword = passwordEncoder.encode(dto.getPassword());
+        member.setPassword(encodedPassword);
 
         memberRepository.save(member);
     }
