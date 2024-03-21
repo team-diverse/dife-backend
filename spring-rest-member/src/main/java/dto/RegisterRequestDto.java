dfb4feceacf3c108290381ce556b916b3a1ac5cf
Seungho <poream3387@gmail.com>
2024-03-21 15:07:01 +0900

    chore(dto): 유저가 보낸 Request 데이터를 가지는 DTO 생성
    개요
    
    - 유저가 Request를 보내면, 그것의 값을 갖고 있는 DTO가 필요하다.
    - 이후 커밋에서 이것을 이용해서 값처리를 해줄 것이다.

---
+++ spring-rest-member/src/main/java/dto/RegisterRequestDto.java
@@ -0,0 +1,40 @@
+package dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequestDto {
+
+    @NotNull()
+    @Email
+    private String email;
+
+    @NotNull()
+    private String student_id;
+
+    @NotNull()
+    private String username;
+
+    @NotNull()
+    private String major;
+
+    @NotNull()
+    private Boolean is_public;
+
+    @NotNull()
+    private Boolean is_korean;
+
+    @NotNull()
+    private String bio;
+
+    @NotNull()
+    private String password;
+}
