7bd3ca9e715c003e010d37d17176b507b5daf6f8
Seungho <poream3387@gmail.com>
2024-03-23 21:38:25 +0900

    feat(file): File 엔티티 세부 수정 및 Format enum 정의
    개요
    
    - 디자인한 ERD에 따라 주요 컬럼 값을 설정한다.
    - 특히, 자동으로 생성 일자를 넣을 수 있도록 @CreationTimestamp 사용
    - 추가로, 파일 포맷을 알려주는 enum을 정의함

--- spring-rest-file/src/main/java/com/dife/file/model/File.java
+++ spring-rest-file/src/main/java/com/dife/file/model/File.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -16,9 +20,22 @@ public class File {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "file_id")
     private Long id;
 
     @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long size;
+
+    @Column(nullable = false)
     private String url;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Format format;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
 }
