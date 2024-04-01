2fa55b6e59fea4cd3d47dbd1664f2c549e82c476
Seungho <poream3387@gmail.com>
2024-04-02 00:57:12 +0900

    refactor: DifeApplication 이름 변경
    - Mono로 합쳐짐에 따라, DifeApplication으로 메인 어플리케이션의 이름을
    변경한다.

--- rest-api/src/main/java/com/dife/api/DifeApplication.java
+++ rest-api/src/main/java/com/dife/api/DifeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MemberApplication {
+public class DifeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
+		SpringApplication.run(DifeApplication.class, args);
 	}
 
 }
