7d2c818a03c07ff898404f43452200f65d11059e
Seungho <poream3387@gmail.com>
2024-04-06 16:40:06 +0900

    ci(test): JUnit Test를 PR 생성 시에 작동하도록 추가한다.
    개요
    
    - 이전 PR에 코드가 누락되어 반영한다

--- rest-api/src/test/java/com/dife/api/DifeApplicationTests.java
+++ rest-api/src/test/java/com/dife/api/DifeApplicationTests.java
@@ -1,13 +1,19 @@
 package com.dife.api;
 
+import com.dife.api.config.AWSConfig;
+import com.dife.api.config.PasswordEncoderConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
 class DifeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    AWSConfig awsConfig;
+    @MockBean
+    PasswordEncoderConfig passwordEncoderConfig;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
