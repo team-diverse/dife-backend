00a034e9cd17af8bc1dce12e8d30d33e6c002429
suyeon <stgood@kookmin.ac.kr>
2024-04-11 21:49:16 +0900

    feat/board-implementation-4 - 로직 변경없는 chore 작업

---
+++ rest-api/src/main/java/com/dife/api/model/dto/PostCreateRequestDto.java
@@ -0,0 +1,29 @@
+package com.dife.api.model.dto;
+
+import com.dife.api.model.BoardCategory;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostCreateRequestDto {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private Boolean is_public;
+
+    @NotNull
+    private BoardCategory boardType;
+
+
+}
