142a5e94fe6498ec031810d4a2485764e8bacb74
Seungho <poream3387@gmail.com>
2024-04-14 14:01:18 +0900

    refactor(ci): Local 환경에서 다른 Bean을 사용할 수 있도록 코드 수정
    개요
    
    - Local 환경인지 아닌지에 따라, Session Token을 필요로 하는지
    구분해야하기 때문에 코드도 달라진다. 따라서 Bean을 다르게 로드하자

--- rest-api/src/main/java/com/dife/api/config/AWSConfig.java
+++ rest-api/src/main/java/com/dife/api/config/AWSConfig.java
@@ -3,6 +3,8 @@ package com.dife.api.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -17,17 +19,28 @@ public class AWSConfig {
     @Value("${spring.aws.secret-key}")
     private String secretKey;
 
-    @Value("${spring.aws.session-token}")
+    @Value("${spring.aws.session-token:#{null}}")
     private String sessionToken;
 
     @Value("${spring.aws.region}")
     private String awsRegion;
 
     @Bean
-    public S3Client S3Bucket() {
+    @Profile("local")
+    public S3Client S3BucketWithSessionToken() {
         return S3Client.builder().credentialsProvider(StaticCredentialsProvider.create(
                         AwsSessionCredentials.create(accessKey, secretKey, sessionToken)))
                 .region(Region.of(awsRegion))
                 .build();
     }
+
+    @Bean
+    @Profile("!local")
+    public S3Client S3BucketWithoutSessionToken() {
+        return S3Client.builder().credentialsProvider(StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of(awsRegion))
+                .build();
+    }
+
 }
