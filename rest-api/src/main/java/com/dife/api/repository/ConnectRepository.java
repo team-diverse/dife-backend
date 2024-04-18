1e37d7d6b5a822f8f26bf2ccf1608e965db505d8
Seungho <poream3387@gmail.com>
2024-04-18 15:49:17 +0900

    feat(connect): POST: /api/connects/ 세부 구현 및 일부 로직 수정
    개요
    
    - 새로운 커넥트를 형성하는 `POST`: `/api/connects`를 구현하자
    
    수정 사항
    
    - `ConnectController`
      - ConnectRequestDto, ConnectResponseDto를 활용하여 더 깔끔한 데이터
    교환이 이루어지도록 함
      - 비즈니스 로직을 ConnectService로 넘기기 위해 Dto와 현재 유저의
    이메일을 넘긴다.
      - 생성 시 CREATED 201을 리턴함
    
    - `ConnectService`
      - Controller에서 받은 정보로, 현재 유저와 보낼 대상의 유저를 가져온다.
      - 만약에 본인 자신에게 커넥트를 보내거나, 이미 다른 유저와의 커넥트가
    존재한다면 에러를 throw
    
    - `ConnectRepository`
      - 두 멤버가 주어졌을 때, 두 멤버 사이에 커넥트가 존재하는지 확인한다.
      - 커넥트는 보낸사람과 받는 사람이 다른 것으로 인식하기 때문에, 커스텀
    쿼리로 사이에 커넥트가 존재하는지 확인한다.
      - 변수명 member1과 member2로 한 이유는, 넘겨주는 시점에서 어떤 멤버가
    보냈고 받았는지 알 수 없기 때문

--- rest-api/src/main/java/com/dife/api/repository/ConnectRepository.java
+++ rest-api/src/main/java/com/dife/api/repository/ConnectRepository.java
@@ -1,7 +1,15 @@
 package com.dife.api.repository;
 
 import com.dife.api.model.Connect;
+import com.dife.api.model.Member;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ConnectRepository extends JpaRepository<Connect, Long> {
+    @Query("SELECT c FROM Connect c WHERE (c.toMember = :member1 AND c.fromMember = :member2) OR (c.fromMember = :member1 AND c.toMember = :member2) ")
+    Optional<Connect> findByMemberPair(@Param("member1") Member member1, @Param("member2") Member member2);
+
 }
