47ebc47895f5a4e6fabc4c320ae3f835449da2b0
Seungho <poream3387@gmail.com>
2024-04-18 17:07:48 +0900

    feat(connect): PATCH request DTO 생성
    개요
    
    - 현재 ConnectRequestDto는 toMember를 기준으로 값을 받는다. 이는
    리퀘스트를 받는 사람이 항상 전달되기 때문인데, PATCH로 일부 컬럼 값을
    업데이트하는 경우에는 다른 사람과의 Connect를 가져오기만 하면 되기
    때문에, 상대방의 id가 필요한데, 이는 꼭 toMember를 의미하진 않는다.
    따라서, Dto를 생성하여 혼란을 방지한다.

---
+++ rest-api/src/main/java/com/dife/api/model/dto/ConnectPatchRequestDto.java
@@ -0,0 +1,17 @@
+package com.dife.api.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConnectPatchRequestDto {
+
+    @NotNull
+    @JsonProperty("member_id")
+    private Long memberId;
+}
