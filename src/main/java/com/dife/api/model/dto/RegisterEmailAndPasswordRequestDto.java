a4dce1de68ac4d1ae5d64c61b7eaca00a669aeff
Koo Su Yeon <124496650+KooSuYeon@users.noreply.github.com>
2024-04-27 23:39:57 +0900

    Fix/swagger signup (#96)
    * fix/swagger-signup: 비밀번호 오류 수정 및 null 처리 DTO 수정
    - 비밀번호 or 설정 사항을 and로 설정해놓은 사항 수정
    - RegisterDetail에서의 null 처리:
    - null 값 가능 : bio, mbti, hobbies, profile_img
    - null 값 불가 : RegisterException 던지도록 (백엔드)
    
    * fix: Spotless check 오류 수정
    - 형식 위반 수정 후 재빌드

--- src/main/java/com/dife/api/model/dto/RegisterEmailAndPasswordRequestDto.java
+++ src/main/java/com/dife/api/model/dto/RegisterEmailAndPasswordRequestDto.java
@@ -24,7 +24,7 @@ public class RegisterEmailAndPasswordRequestDto {
 	@NotNull(message = "비밀번호를 입력해주세요!")
 	@Schema(description = "비밀번호", example = "password")
 	@Pattern(
-			regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+			regexp = "(?=.*[0-9a-zA-Z\\\\W]).{8,20}",
 			message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
 	private String password;
 }
