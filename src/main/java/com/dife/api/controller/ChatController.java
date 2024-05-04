e874352fe13bfe5e454b490a8d06fd23c5d12953
suyeon <stgood@kookmin.ac.kr>
2024-05-04 19:09:14 +0900

    feat/chat-group-http-implementation-3: 그룹 채팅방 단계별 생성 추가
    - createGroupChatroom(POST):
    - 채팅방 명, 한줄소개, 프로필 사진
    - DTO -> Param 입력으로 변경
    - registerDetail(PUT):
    - 태그, 최소/최대 인원수 입력, 언어, 목적, 공개/비공개여부, 비밀번호
    - 로직은 단계별 회원가입과 동일
    
    - getChatroom:
    - 채팅방 고유 ID로 가져옴
    - swagger 설정 추가

--- src/main/java/com/dife/api/controller/ChatController.java
+++ src/main/java/com/dife/api/controller/ChatController.java
@@ -1,41 +1,78 @@
 package com.dife.api.controller;
 
-import com.dife.api.model.Chatroom;
+import com.dife.api.model.*;
 import com.dife.api.model.dto.GroupChatroomRequestDto;
 import com.dife.api.model.dto.GroupChatroomResponseDto;
 import com.dife.api.service.ChatroomService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/chats")
 @Slf4j
-@Tag(name = "Chat API", description = "Chat API")
 public class ChatController {
 
 	private final ChatroomService chatroomService;
 
 	@Operation(summary = "그룹 채팅방 생성", description = "사용자가 그룹 채팅방 생성")
-	@PostMapping
-	public ResponseEntity<GroupChatroomResponseDto> createGroupChatroom(
-			@RequestBody(
-							description = "채팅방 이름, 한줄소개, 최소 인원수, 최대 인원수를 포함하는 그룹 채팅방 생성 데이터",
-							required = true,
-							content = @Content(schema = @Schema(implementation = GroupChatroomRequestDto.class)))
-					GroupChatroomRequestDto dto) {
+	@PostMapping(value = "/", consumes = "multipart/form-data")
+	@ApiResponse(
+			responseCode = "201",
+			description = "그룹 생성방1 성공 예시",
+			content = {
+				@Content(
+						mediaType = "application/json",
+						schema = @Schema(implementation = GroupChatroomRequestDto.class))
+			})
+	public ResponseEntity<GroupChatroomRequestDto> createGroupChatroom(
+			@RequestParam(value = "name", required = false) String name,
+			@RequestParam("description") String description,
+			@RequestParam("profile_img") MultipartFile profile_img) {
 
-		Chatroom chatroom = chatroomService.createGroupChatroom(dto);
+		Chatroom chatroom = chatroomService.createGroupChatroom(name, description, profile_img);
 
+		return ResponseEntity.status(HttpStatus.CREATED).body(new GroupChatroomRequestDto(chatroom));
+	}
+
+	@PutMapping(value = "/{id}", consumes = "multipart/form-data")
+	@ApiResponse(
+			responseCode = "201",
+			description = "그룹 채팅방2 성공 예시",
+			content = {
+				@Content(
+						mediaType = "application/json",
+						schema = @Schema(implementation = GroupChatroomResponseDto.class))
+			})
+	public ResponseEntity<GroupChatroomResponseDto> registerDetail(
+			@RequestParam(value = "tags", required = false) Set<String> tags,
+			@RequestParam(value = "min_count", required = false) Integer min_count,
+			@RequestParam(value = "max_count", required = false) Integer max_count,
+			@RequestParam(value = "languages", required = false) Set<String> languages,
+			@RequestParam(value = "purposes", required = false) Set<String> purposes,
+			@RequestParam(value = "is_public", required = false) Boolean is_public,
+			@RequestParam(value = "password", required = false) String password,
+			@PathVariable Long id) {
+
+		Chatroom chatroom =
+				chatroomService.registerDetail(
+						tags, min_count, max_count, languages, purposes, is_public, password, id);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new GroupChatroomResponseDto(chatroom));
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<GroupChatroomResponseDto> getGroupChatroom(@PathVariable Long id) {
+		Chatroom chatroom = chatroomService.getChatroom(id);
+		return ResponseEntity.status(HttpStatus.OK).body(new GroupChatroomResponseDto(chatroom));
 	}
 }
