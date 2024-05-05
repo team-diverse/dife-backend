df0f336cb2d719812fe8153d4b0c44c0c52bcf5b
Seungho <poream3387@gmail.com>
2024-05-06 01:34:12 +0900

    test(jwt): JWTUtilTest - 이외의 메서드 테스트 작성
    - Mockito의 `@Mock` 기능을 활용하기 위해
    `@ExtendWith(MockitoExtension.class)` 활용
    
    - HttpServletRequest를 Mock하여 Http request mock
    
    - 특히나, `resolveToken`의 함수는 테스트해야할 여러 케이 있으므로,
    Nested Test를 사용하여 시나리오를 구분하여 작성한다.

--- src/test/java/com/dife/api/jwt/JWTUtilTest.java
+++ src/test/java/com/dife/api/jwt/JWTUtilTest.java
@@ -1,20 +1,28 @@
 package com.dife.api.jwt;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class JWTUtilTest {
 	private static JWTUtil jwtUtil;
 	private static SecretKey secretKey;
 
+	@Mock private HttpServletRequest request;
 
 	@BeforeAll
 	public static void setUp() {
@@ -43,4 +51,47 @@ public class JWTUtilTest {
 		assertNotNull(claims.getExpiration());
 	}
 
+	@Test
+	public void getEmail_ShouldReturnEmail_WhenTokenPassed() {
+		String email = "user@example.com";
+		String token = jwtUtil.createAccessJwt(email, "user", 1000L * 60 * 60);
+
+		assertEquals(email, jwtUtil.getEmail(token));
+	}
+
+	@Test
+	public void getRole_ShouldReturnRole_WhenTokenPassed() {
+		String role = "admin";
+		String token = jwtUtil.createAccessJwt("user@example.com", role, 1000L * 60 * 60);
+
+		assertEquals(role, jwtUtil.getRole(token));
+	}
+
+	@Test
+	public void isExpired_ShouldReturnExpiredJwtException_WhenTokenExpired() {
+		String token = jwtUtil.createAccessJwt("user@example.com", "user", -1000L);
+
+		assertThrows(ExpiredJwtException.class, () -> jwtUtil.isExpired(token));
+	}
+
+	@Nested
+	class ResolveTokenMethod {
+		@Test
+		public void shouldReturnToken_WhenValidToken() {
+			when(request.getHeader("Authorization")).thenReturn("Bearer validToken");
+			assertEquals("validToken", jwtUtil.resolveToken(request));
+		}
+
+		@Test
+		public void shouldReturnNull_WhenInvalidToken() {
+			when(request.getHeader("Authorization")).thenReturn("Invalid");
+			assertNull(jwtUtil.resolveToken(request));
+		}
+
+		@Test
+		public void shouldReturnNull_WhenNull() {
+			when(request.getHeader("Authorization")).thenReturn(null);
+			assertNull(jwtUtil.resolveToken(request));
+		}
+	}
 }
