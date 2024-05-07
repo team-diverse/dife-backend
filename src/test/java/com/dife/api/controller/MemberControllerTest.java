d4d6ab942e01e26f9b5c2041e9efacd945139d01
Seungho <poream3387@gmail.com>
2024-05-07 13:37:24 +0900

    test(member): MemberController Test 초기 작성 및 회원가입 테스트 작성
    개요
    
    - MemberController에 대한 Unit test를 작성하자
    
    수정 사항
    
    - MemberControllerTest.java 생성
    - `@Mock`: 없는 것을 있게 한다는 뜻
    - `@InjectMocks`: 테스트의 대상인 MemberController를 의미하며,
    memberService 등 필요한 Mock을 주입함
    - @BeforeEach: 이전의 @BeforeAll과는 다르게, mockMVC 같은 경우는, 테스트
    함수가 돌아갈때마다, 새로 셋팅을 해줘야함. 그 이유는 `setup`에서 정확한
    테스트를 위해 `memberController`를 독립된 환경에서 테스트할 수 있도록
    분리하기 위함임
    
    - registerEmailAndPassword 테스트 작성
      - mockMvc로 요청한 멤버와 controller 함수를 실행했을 때, 동일한지
    확인할 수 있도록 취함

---
+++ src/test/java/com/dife/api/controller/MemberControllerTest.java
@@ -0,0 +1,60 @@
+package com.dife.api.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.dife.api.model.Member;
+import com.dife.api.model.dto.RegisterEmailAndPasswordRequestDto;
+import com.dife.api.service.MemberService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberControllerTest {
+
+	private MockMvc mockMvc;
+
+	@Mock private MemberService memberService;
+
+	@InjectMocks private MemberController memberController;
+
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(memberController).build();
+	}
+
+	@Test
+	public void registerEmailAndPassword_ShouldReturn201_WhenEmailPasswordIsPassed()
+			throws Exception {
+		RegisterEmailAndPasswordRequestDto requestDto =
+				new RegisterEmailAndPasswordRequestDto("email@example.com", "password123");
+		String reqBody = new ObjectMapper().writeValueAsString(requestDto);
+
+		Member member = new Member();
+		member.setId(1L);
+		member.setEmail(requestDto.getEmail());
+		member.setPassword(requestDto.getPassword());
+
+		given(memberService.registerEmailAndPassword(any(RegisterEmailAndPasswordRequestDto.class)))
+				.willReturn(member);
+
+		mockMvc
+				.perform(
+						post("/api/members/register").contentType(MediaType.APPLICATION_JSON).content(reqBody))
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.success").value(true))
+				.andExpect(jsonPath("$.member_id").value(member.getId()))
+				.andExpect(jsonPath("$.email").value("email@example.com"));
+	}
+}
