6984357b1eafd4b7ac43b038fe695384e0c39bc4
Seungho <poream3387@gmail.com>
2024-05-09 23:57:28 +0900

    chore(redis): Redis Subscribe 리스너 등록
    개요 및 수정 사항
    
    - RedisConfig.java
      - MessageListener Adapter, Container 추가 -> chat topic에 관련하여,
    redis에 구독한다.
    
    - RedisSubscriber.java
      - RedisSubscriber는 Redis로부터 메시지를 받았을 때, 어떻게 할지에 대한
    리스너임

--- src/main/java/com/dife/api/config/RedisConfig.java
+++ src/main/java/com/dife/api/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.dife.api.config;
 
 import com.dife.api.redis.RedisPublisher;
+import com.dife.api.redis.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,8 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -45,5 +48,18 @@ public class RedisConfig {
 	@Bean
 	public RedisPublisher redisPublisher(ChannelTopic topic) {
 		return new RedisPublisher(redisTemplate(), topic);
+	}
+
+	@Bean
+	public MessageListenerAdapter messageListener() {
+		return new MessageListenerAdapter(new RedisSubscriber());
+	}
+
+	@Bean
+	public RedisMessageListenerContainer redisContainer() {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(lettuceConnectionFactory());
+		container.addMessageListener(messageListener(), topic());
+		return container;
 	}
 }
---
+++ src/main/java/com/dife/api/redis/RedisSubscriber.java
@@ -0,0 +1,15 @@
+package com.dife.api.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RedisSubscriber implements MessageListener {
+	@Override
+	public void onMessage(Message message, byte[] pattern) {
+		log.info("Message received: " + message.toString());
+	}
+}
