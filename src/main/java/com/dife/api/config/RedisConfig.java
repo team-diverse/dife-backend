c693e91bf8df02172d7c401e3ff37261e631e64f
Seungho <poream3387@gmail.com>
2024-05-09 21:44:08 +0900

    chore(redis): 로컬 redis 세팅을 작성하고 Publish를 구현한다.
    개요
    
    - RedisConfig
      - Redis와 Spring의 커넥션을 위한 Config
      - LettuceConnectionFactory를 사용해서 연결 생성
      - RedisTemplate으로 String, Object 형태로 Message를 넣을 수 있도록
    템플릿 작성
    
    - RedisPublisher
      - Redis관련 publish를 해야할때마다 호출할 수 있는 클래스/함수
      - publish로 필요한 정보 publish 하면 됨

--- src/main/java/com/dife/api/config/RedisConfig.java
+++ src/main/java/com/dife/api/config/RedisConfig.java
@@ -1,4 +1,49 @@
 package com.dife.api.config;
 
+import com.dife.api.redis.RedisPublisher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@EnableRedisRepositories
+@Configuration
 public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
+	@Bean
+	public LettuceConnectionFactory lettuceConnectionFactory() {
+		RedisStandaloneConfiguration configuration =
+				new RedisStandaloneConfiguration(redisHost, redisPort);
+		return new LettuceConnectionFactory(configuration);
+	}
+
+	@Bean
+	public RedisTemplate<String, String> redisTemplate() {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(lettuceConnectionFactory());
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new StringRedisSerializer());
+		return template;
+	}
+
+	@Bean
+	public ChannelTopic topic() {
+		return new ChannelTopic("chat");
+	}
+
+	@Bean
+	public RedisPublisher redisPublisher(ChannelTopic topic) {
+		return new RedisPublisher(redisTemplate(), topic);
+	}
 }
--- src/main/java/com/dife/api/redis/RedisPublisher.java
+++ src/main/java/com/dife/api/redis/RedisPublisher.java
@@ -1,4 +1,15 @@
 package com.dife.api.redis;
 
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+
+@AllArgsConstructor
 public class RedisPublisher {
+	private RedisTemplate<String, String> redisTemplate;
+	private ChannelTopic topic;
+
+	public void publish(String message) {
+		redisTemplate.convertAndSend(topic.getTopic(), message);
+	}
 }
