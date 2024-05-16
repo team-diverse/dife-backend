f9ec3dc38772e552a947649e5b1b5e620e758c3d
Seungho <poream3387@gmail.com>
2024-05-16 09:03:09 +0900

    test(s3): 로컬 S3 테스트 컨테이너 설정
    수정 사항
    - 기존의 AWSConfig에 !test를 추가하여 테스트가 아닐때만 Bean이
    로드되도록 설정
    
    - LocalAWSConfig를 생성하여 Test용 AWSConfig 생성
      - LocalStack 컨테이너를 사용하여 테스트시  도커를 띄우는 것
      - initMethod와 destroyMethod는 Bean 생성/종료 시 LocalStackContainer의
    container start와 stop을 해서 테스트에서만 사용되도록 함
      - TestConfiguration으로 테스트용도로 설정

---
+++ src/test/java/com/dife/api/config/LocalAWSConfig.java
@@ -0,0 +1,31 @@
+package com.dife.api.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@TestConfiguration
+public class LocalAWSConfig {
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public LocalStackContainer localStackContainer() {
+		return new LocalStackContainer(DockerImageName.parse("localstack/localstack:3"))
+				.withServices(LocalStackContainer.Service.S3);
+	}
+
+	@Bean
+	public S3Client s3Client(LocalStackContainer localStackContainer) {
+		return S3Client.builder()
+				.endpointOverride(localStackContainer.getEndpointOverride(LocalStackContainer.Service.S3))
+				.credentialsProvider(
+						StaticCredentialsProvider.create(
+								AwsBasicCredentials.create(
+										localStackContainer.getAccessKey(), localStackContainer.getSecretKey())))
+				.region(Region.of(localStackContainer.getRegion()))
+				.build();
+	}
+}
