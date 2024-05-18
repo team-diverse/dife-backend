3fb63467af19803a83883563cf679985c537f8ab
Koo Su Yeon <124496650+KooSuYeon@users.noreply.github.com>
2024-05-18 16:14:18 +0900

    Test/chatroom controller : 채팅방 생성 JUnit, Mockito 테스트 작성 (#108)
    * refactor: ChatController 클래스명 수정
    - 채팅방 생성및 관리 로직이 들어있으므로 클래스 명칭 변경 -> ChatroomController
    - 로직 변경 없음
    
    * test/chatroom-controller-1:
    그룹 채팅방 생성 Test
    - Setup: 여러 익셉션 테스트할 chatroomController 세팅
    
    - 그룹 채팅방 생성 성공 201 테스트:
    - 파라미터로 채팅방 생성 요소를 주입받으므로 이를 고려한 mockMvc의 contentType 설정 -> MediaType.MULTIPART_FORM_DATA
    
    - 그룹 채팅방 생성 중복 채팅방 이름 409 에러 테스트:
    - 같은 채팅방 이름을 가진 채팅방을 두번 생성하는 것이 아닌 ChatroomDuplication이 넘어왔을 때 어떻게 반응하는지를 위한 테스트
    
    - 그룹 채팅방 생성 요소 NULL시 422 에러 테스트:
    - name과 description이 빈 문자열일 경우 반응 테스트

--- src/main/java/com/dife/api/controller/ChatroomController.java
+++ src/main/java/com/dife/api/controller/ChatroomController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/chats")
 @Slf4j
-public class ChatController {
+public class ChatroomController {
 
 	private final ChatroomService chatroomService;
 
