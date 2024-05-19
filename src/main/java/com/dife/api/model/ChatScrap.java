70081de60e672f73fa6ee793e5b27384a27accf2
suyeon <stgood@kookmin.ac.kr>
2024-05-19 23:07:36 +0900

    feat/chat-single-3: 채팅 스크랩 기능 세팅 - 웹소켓 엔드포인트 : sub/chatroom/scrap - Chatroom : - 저장되는 채팅과 동일하게 일대다로 스크랩 엔티티와 연결

---
+++ src/main/java/com/dife/api/model/ChatScrap.java
@@ -0,0 +1,32 @@
+package com.dife.api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chatscrap")
+public class ChatScrap {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(max = 300)
+	private String message;
+
+	@ManyToOne
+	@JoinColumn(name = "chatroom_id")
+	private Chatroom chatroom;
+
+	private String sender;
+}
