32c58268c2e8a9268600fcfc8ccc7e18df625a30
suyeon <stgood@kookmin.ac.kr>
2024-05-20 17:39:44 +0900

    chore: 북마크 이름 변경 - 로직 변경은 없습니다!

--- src/main/java/com/dife/api/service/ChatroomService.java
+++ src/main/java/com/dife/api/service/ChatroomService.java
@@ -2,8 +2,13 @@ package com.dife.api.service;
 
 import com.dife.api.exception.*;
 import com.dife.api.model.*;
+<<<<<<< HEAD
 import com.dife.api.model.dto.ChatDto;
 import com.dife.api.model.dto.ChatScraplistDto;
+=======
+import com.dife.api.model.dto.BookmarklistDto;
+import com.dife.api.model.dto.ChatlistDto;
+>>>>>>> 43cc8c5 (chore: 북마크 이름 변경)
 import com.dife.api.repository.*;
 import java.util.HashSet;
 import java.util.List;
@@ -26,8 +31,12 @@ public class ChatroomService {
 	private final LanguageRepository languageRepository;
 	private final GroupPurposesRepository groupPurposesRepository;
 	private final ChatRepository chatRepository;
+<<<<<<< HEAD
 	private final ChatScrapRepository chatScrapRepository;
 	private final ModelMapper modelMapper;
+=======
+	private final BookmarkRepository bookmarkRepository;
+>>>>>>> 43cc8c5 (chore: 북마크 이름 변경)
 
 	private final FileService fileService;
 
@@ -113,19 +122,19 @@ public class ChatroomService {
 		return chat;
 	}
 
-	public List<ChatScraplistDto> getScraps(Long id) {
+	public List<BookmarklistDto> getBookmarks(Long id) {
 
-		List<ChatScrap> scraps = chatScrapRepository.findScrapsByChatroomId(id);
+		List<Bookmark> scraps = bookmarkRepository.findScrapsByChatroomId(id);
 
-		return scraps.stream().map(ChatScraplistDto::new).collect(Collectors.toList());
+		return scraps.stream().map(BookmarklistDto::new).collect(Collectors.toList());
 	}
 
-	public ChatScrap getScrap(Long room_id, Long scrap_id) {
-		ChatScrap chatScrap =
-				chatScrapRepository
-						.findByChatroomIdAndId(room_id, scrap_id)
+	public Bookmark getBookmark(Long room_id, Long bookmark_id) {
+		Bookmark bookmark =
+				bookmarkRepository
+						.findByChatroomIdAndId(room_id, bookmark_id)
 						.orElseThrow(() -> new ChatroomException("존재하지 않는 스크랩입니다!"));
-		return chatScrap;
+		return bookmark;
 	}
 
 	public Chatroom registerDetail(
