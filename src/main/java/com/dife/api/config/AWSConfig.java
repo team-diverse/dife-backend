05efdfa18f0f115dce388e236006c8c0ea68dce3
Seungho Lee <poream3387@gmail.com>
2024-05-25 15:13:11 +0900

    build(test): testcontainer로 S3, Redis 추가 (#114)
    * build(test): testcontainer, localstack 라이브러리 추가
    
    개요
    
    - S3와 Redis등 현재 테스트에서 에러가 나고 있다. 이는, 테스트 환경에서
    S3와 Redis 인스턴스를 받지 못하는 상황에서 발생하는 것이기에 로컬
    테스트에서 이들을 만들어주도록 하는 testcontainer를 사용하자
    
    수정 사항
    
    - allow-bean-definition-overriding: true -> test bean이 정의되어있다면
    그것을 사용하도록 하는 설정이다.
    
    - aws.bucket-name 과 jwt.secret을 test 환경의 application.yml에 미리
    설정해준다
    
    * test(s3): 로컬 S3 테스트 컨테이너 설정
    
    수정 사항
    - 기존의 AWSConfig에 !test를 추가하여 테스트가 아닐때만 Bean이
    로드되도록 설정
    
    - LocalAWSConfig를 생성하여 Test용 AWSConfig 생성
      - LocalStack 컨테이너를 사용하여 테스트시  도커를 띄우는 것
      - initMethod와 destroyMethod는 Bean 생성/종료 시 LocalStackContainer의
    container start와 stop을 해서 테스트에서만 사용되도록 함
      - TestConfiguration으로 테스트용도로 설정

--- src/main/java/com/dife/api/config/AWSConfig.java
+++ src/main/java/com/dife/api/config/AWSConfig.java
@@ -11,6 +11,7 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@Profile("!test")
 public class AWSConfig {
 
 	@Value("${spring.aws.access-key}")
--- src/main/java/com/dife/api/config/SecurityConfig.java
+++ src/main/java/com/dife/api/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.dife.api.jwt.LoginFilter;
 import com.dife.api.repository.MemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@Profile("!test")
 public class SecurityConfig {
 
 	private final AuthenticationConfiguration authenticationConfiguration;
