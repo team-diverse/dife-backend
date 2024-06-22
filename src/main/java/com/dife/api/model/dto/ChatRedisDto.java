2cfdc17f16280d7198fad28f7da0b62701b6c3ad
Seungho Lee <poream3387@gmail.com>
2024-06-23 03:02:50 +0900

    chore(chat): ChatRedisDto 추가
    개요
    
    - 현재 ChatRequestDto와 ChatResponseDto를 사용중인데, Redis로 전달 및
    받아오는 것도 ChatRequestDto로 하고 있다. 이런 경우에,
    ChatRequestDto에서 Chat id 등이 필요한 경우에 추가해줄 순
    있으나, RequestDto 자체에 Chat id가 있는 것은 모순이므로(Request
    당시에는 채팅이 저장되지 않아서 ID가 없음) 문제가
    생긴다. 따라서 Redis 전용 Dto 생성을 한다.

---
+++ src/main/java/com/dife/api/model/dto/ChatRedisDto.java
@@ -0,0 +1,27 @@
+package com.dife.api.model.dto;
+
+import com.dife.api.model.Chatroom;
+import com.dife.api.model.Member;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatRedisDto {
+	private Long id;
+
+	@NotNull
+	@Size(max = 300)
+	private String message;
+
+	private Member member;
+	private Chatroom chatroom;
+	private LocalDateTime created;
+}
