983ac16a965ff5fd19898b3fa942cbf1b9eb3061
Seungho Lee <poream3387@gmail.com>
2024-06-23 12:54:17 +0900

    refactor(redis): Redis의 ObjectMapper 추가 및 리팩토링
    개요 및 수정사항
    - ObjectMapper에서 ISO형식의 time을 serialize 할 수 있도록
    LocalDateTimeSerializer로 추가
    - ObjectMapper가 반복되는데,  Redis 내부의 Bean으로 등록해서
    재활용하자, 그에 따라, RedisConfig에서의 일부 Bean의 parameter 변경
    - 이전 commit에서 만든 ChatRedisDto를 활용하여, member, chatroom을
    함께 전달하여 해당 정보를를 채팅을 받았을 때 함께 받을 수 있도록
    Redis로 전전달 시킨다.
    - 로컬 Redis Integration testing 코드도 덩달아 알맞게 수정
    
    - 정보 형식 변경 및 리팩토링 이외에 로직 변경은 없음

--- src/main/java/com/dife/api/redis/RedisPublisher.java
+++ src/main/java/com/dife/api/redis/RedisPublisher.java
@@ -1,27 +1,24 @@
 package com.dife.api.redis;
 
-import com.dife.api.model.dto.ChatRequestDto;
+import com.dife.api.model.dto.ChatRedisDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class RedisPublisher {
-	private RedisTemplate<String, String> redisTemplate;
-	private ChannelTopic topic;
+	private final RedisTemplate<String, String> redisTemplate;
+	private final ChannelTopic topic;
+	private final ObjectMapper objectMapper;
 
-	public void publish(ChatRequestDto dto) throws JsonProcessingException {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModule(new JavaTimeModule());
-		String jsonMessage = mapper.writeValueAsString(dto);
-
+	public void publish(ChatRedisDto dto) throws JsonProcessingException {
+		String jsonMessage = objectMapper.writeValueAsString(dto);
 		redisTemplate.convertAndSend(topic.getTopic(), jsonMessage);
 	}
 }
