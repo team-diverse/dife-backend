10035c6959ce4b881c03c7d3ad9a0f33a2c6186c
suyeon <stgood@kookmin.ac.kr>
2024-07-02 10:51:35 -0700

    test/member-login-1: 로그인 테스트 코드 작성 - 메서드이름 규칙, Service 사용시 given, Mock 생성 - 로그인 성공 시 200, 실패시 500 에러 발생시킴

--- src/test/java/com/dife/api/controller/MemberControllerTest.java
+++ src/test/java/com/dife/api/controller/MemberControllerTest.java
@@ -10,6 +10,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.dife.api.GlobalExceptionHandler;
 import com.dife.api.exception.DuplicateMemberException;
 import com.dife.api.model.Member;
+import com.dife.api.model.dto.LoginDto;
+import com.dife.api.model.dto.LoginSuccessDto;
 import com.dife.api.model.dto.RegisterEmailAndPasswordRequestDto;
 import com.dife.api.model.dto.RegisterResponseDto;
 import com.dife.api.service.MemberService;
@@ -21,7 +23,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -84,6 +88,60 @@ public class MemberControllerTest {
 									.content(reqBody))
 					.andExpect(status().isConflict())
 					.andExpect(jsonPath("$.success").value(false));
+		}
+	}
+
+	@Nested
+	class loginMethod {
+
+		@Test
+		public void shouldReturn200_WhenLoginIsPassed() throws Exception {
+			LoginDto requestDto = new LoginDto();
+			requestDto.setEmail("email@gmail.com");
+			requestDto.setPassword("password123");
+			String reqBody = new ObjectMapper().writeValueAsString(requestDto);
+
+			LoginSuccessDto responseDto =
+					new LoginSuccessDto(
+							1L,
+							"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+							"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
+
+			ResponseEntity<LoginSuccessDto> responseEntity =
+					new ResponseEntity<>(responseDto, HttpStatus.OK);
+
+			given(memberService.login(any(LoginDto.class))).willReturn(responseEntity);
+
+			mockMvc
+					.perform(
+							post("/api/members/login").contentType(MediaType.APPLICATION_JSON).content(reqBody))
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("member_id").value(1L))
+					.andExpect(
+							jsonPath("$.accessToken")
+									.value(
+											"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"))
+					.andExpect(
+							jsonPath("$.refreshToken")
+									.value(
+											"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"));
+		}
+
+		@Test
+		public void shouldReturn500_WhenPasswordIsWrong() throws Exception {
+			LoginDto requestDto = new LoginDto();
+			requestDto.setEmail("email@gmail.com");
+			requestDto.setPassword("wrong_password");
+
+			String reqBody = new ObjectMapper().writeValueAsString(requestDto);
+
+			given(memberService.login(any(LoginDto.class)))
+					.willReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+
+			mockMvc
+					.perform(
+							post("/api/members/login").contentType(MediaType.APPLICATION_JSON).content(reqBody))
+					.andExpect(status().isInternalServerError());
 		}
 	}
 }
