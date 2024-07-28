61ccb7f5aa39dac833146e355cf1b039613f7bb6
suyeon <stgood@kookmin.ac.kr>
2024-07-29 06:33:23 +0900

    feat/notification-expo-3: 알림 Controller 설정 및 게시글 생성 시 자동 알림 테스트 - Controller, Service: - API 설정 - 토큰 생성 ("/push") - 알림 생성 ("/send") - 알림 전체 조회 (속한 토큰까지 조회, GET) - PostService: - DTO 생성 후 자동 알림 테스트 코드 작성

--- src/main/java/com/dife/api/controller/NotificationController.java
+++ src/main/java/com/dife/api/controller/NotificationController.java
@@ -1,16 +1,17 @@
 package com.dife.api.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+import com.dife.api.model.dto.NotificationRequestDto;
 import com.dife.api.model.dto.NotificationResponseDto;
+import com.dife.api.model.dto.NotificationTokenRequestDto;
+import com.dife.api.model.dto.NotificationTokenResponseDto;
 import com.dife.api.service.NotificationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,15 +20,26 @@ public class NotificationController {
 
 	private final NotificationService notificationService;
 
-	@GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public SseEmitter streamNotifications(Authentication auth) {
-		return notificationService.createEmitter(auth.getName());
-	}
-
 	@GetMapping
 	public ResponseEntity<List<NotificationResponseDto>> getNotifications(Authentication auth) {
 		List<NotificationResponseDto> responseDtos =
 				notificationService.getNotifications(auth.getName());
 		return ResponseEntity.ok(responseDtos);
+	}
+
+	@PostMapping("/push")
+	public ResponseEntity<NotificationTokenResponseDto> createNotificationToken(
+			@RequestBody NotificationTokenRequestDto requestDto, Authentication auth) {
+		NotificationTokenResponseDto responseDto =
+				notificationService.sendNotificationToken(auth.getName(), requestDto);
+		return ResponseEntity.status(CREATED).body(responseDto);
+	}
+
+	@PostMapping("/send")
+	public ResponseEntity<NotificationResponseDto> createNotification(
+			@RequestBody NotificationRequestDto requestDto, Authentication auth) {
+		NotificationResponseDto responseDto =
+				notificationService.sendNotification(auth.getName(), requestDto);
+		return ResponseEntity.status(CREATED).body(responseDto);
 	}
 }
