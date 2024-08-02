567abe8b88b4c9d163984e02bdb530672f3d8759
Koo Su Yeon <124496650+KooSuYeon@users.noreply.github.com>
2024-08-02 16:09:56 +0900

    Fix/community: 프론트에서 필요한 커뮤니티 정보를 응답 DTO에 추가 (#176)
    * fix/community-1: 게시글 조회 응답 DTO에 댓글 갯수를 표시한다.
    - 단일 게시글 조회 시 / 게시글 그룹별 조회 시 댓글 갯수는 응답 DTO에 포함된다.
    
    * fix/community-2: 댓글 조회시 본인이 종속되어 있는 게시글의 정보가 응답 DTO에 포함된다.
    - 8/10 이후에 응답 DTO 초기값 정할 지 여부 결정 예정

--- src/main/java/com/dife/api/model/dto/CommentResponseDto.java
+++ src/main/java/com/dife/api/model/dto/CommentResponseDto.java
@@ -2,6 +2,7 @@ package com.dife.api.model.dto;
 
 import com.dife.api.model.Comment;
 import com.dife.api.model.Member;
+import com.dife.api.model.Post;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
@@ -20,11 +21,13 @@ public class CommentResponseDto {
 
 	private Boolean isPublic;
 
+	private Boolean isLiked;
+
+	private Post post;
+
 	private Comment parentComment;
 
 	private Integer likesCount;
-
-	private Boolean isLiked;
 
 	private Integer bookmarkCount;
 
