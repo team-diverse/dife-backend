c820eaaddd5d87630d9bcab9bad15e846ef32066
Seungho Lee <poream3387@gmail.com>
2024-08-07 00:11:52 +0900

    ci(ecs): AWS ECS를 활용한 개선된 Pipeline 구성 및 설정 (#171)
    개요
    
    - 기존에 EC2만을 사용해서 Pipeline을 구성했는데, ECS로 이전함에 따라,
    알맞게 파이프를 구성하자
    
    수정사항
    
    - 기존에 넘겨주었던 많은 Secret 환경 변수들을 AWS에 안전하게 저장하기
    때문에 pipeline에 굳이 환경변수를 노출할 필요가 없어져서 삭제한다.
    - Prod에 Push를 날리면, ECS에서 Task Definition을 새로 만들어서 새로운 컨테이너를 돌린다.
    
    - Spring Actuator를 install하여, /health path에서 Health Check가
    이루어지도록 한다. 이는, 서버가 다운되었는지 확인하는 Load balancer의
    요구 사항이므로 추가한다.
      - 추가로, 해당 path를 누구나 접근할 수 있게 열어놓는다.
    
    - 아마존에서 배포 시에는, 아마존의 secretKey, accessKey등의 키가 없어도
    S3 접근이 가능하다. 따라서 이 부분을 삭제하고 로컬에서는 테스트할때
    필요하므로, 로컬 환경에서만 필요하도록 설정해놓는다.

--- src/main/java/com/dife/api/config/AWSConfig.java
+++ src/main/java/com/dife/api/config/AWSConfig.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -15,10 +15,10 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Profile("!test")
 public class AWSConfig {
 
-	@Value("${spring.aws.access-key}")
+	@Value("${spring.aws.access-key:#{null}")
 	private String accessKey;
 
-	@Value("${spring.aws.secret-key}")
+	@Value("${spring.aws.secret-key:#{null}")
 	private String secretKey;
 
 	@Value("${spring.aws.session-token:#{null}}")
@@ -42,18 +42,27 @@ public class AWSConfig {
 	@Profile("!local")
 	public S3Client S3BucketWithoutSessionToken() {
 		return S3Client.builder()
-				.credentialsProvider(
-						StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+				.credentialsProvider(DefaultCredentialsProvider.create())
 				.region(Region.of(awsRegion))
 				.build();
 	}
 
 	@Bean
-	public S3Presigner presigner() {
+	@Profile("local")
+	public S3Presigner presignerWithSessionToken() {
 		return S3Presigner.builder()
 				.credentialsProvider(
 						StaticCredentialsProvider.create(
 								AwsSessionCredentials.create(accessKey, secretKey, sessionToken)))
+				.region(Region.of(awsRegion))
+				.build();
+	}
+
+	@Bean
+	@Profile("!local")
+	public S3Presigner presigner() {
+		return S3Presigner.builder()
+				.credentialsProvider(DefaultCredentialsProvider.create())
 				.region(Region.of(awsRegion))
 				.build();
 	}
