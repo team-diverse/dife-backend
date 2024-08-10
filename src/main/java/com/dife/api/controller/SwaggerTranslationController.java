c05d6c8d6bbf13a8819eee029eb26be3e3262858
Koo Su Yeon <124496650+KooSuYeon@users.noreply.github.com>
2024-08-10 14:45:50 +0900

    feat/translation-1: DeepL API를 활용한 번역 서비스 (#181)
    * fix/community-1: 게시글 조회 응답 DTO에 댓글 갯수를 표시한다.
    - 단일 게시글 조회 시 / 게시글 그룹별 조회 시 댓글 갯수는 응답 DTO에 포함된다.
    
    * fix/community-2: 댓글 조회시 본인이 종속되어 있는 게시글의 정보가 응답 DTO에 포함된다.
    - 8/10 이후에 응답 DTO 초기값 정할 지 여부 결정 예정
    
    * fix/member-1: 회원정보 각각의 필드값에 따른 수정을 가능하게 하자
    
    - Set형태였던 hobbies, languages들을 null 값이 아닐 때에만 수정이 들어가며 null일 때에는 기존 값들이 들어가도록 함
    - 기존의 /id API로 들어갔던 수정 메서드를 토큰 인증을 이용한 API로 수정함
    
    * feat/translation-1: DeepL API를 활용한 번역 서비스
    
    - TranslationController, TranslationRequestDto:
    - 요청 DTO :
    - text : 번역하고자 하는 내용, 50자 내외여야 함, 한 줄에 여러 개의 문장 보낼 수 있음, List 형태로 번역됨
    - target_lang : 받아보고 싶은 언어코드
    - 영어(EN), 중국어(ZH), 스페인어(ES), 일본어(JA), 한국어(KO)
    
    - TranslationService, TranslationResponseDto:
    - DeepL API가 자동으로 번역하고자 하는 언어의 코드를 분석한 후 응답 DTO에 포함시킴
    
    - application.yml:
    - .env의 TRANSLATION_API_KEY 값에 본인의 api key 값을 추가해야 함
    
    * feat/translation-2: 북마크 서비스에 번역 API 적용 - 엔티티
    
    - Bookmark:
    - 빠졌던 Created 추가
    - 한 개의 번역을 여러 사용자/북마크가 가질 수 있고 한 개의 북마크는 여러 줄의 번역본 (한 개의 번역이 50줄 제한인데 게시글의 번역의 경우 무조건 이를 넘길 것이라고 판단해서) 을 가질 수 있음 -> 다대다 관계
    
    - import.sql: 북마크 DummyData 추가
    
    * feat/translation-3: 북마크 서비스에 번역 API 적용 - 로직
    
    - 요청 DTO에 bookmarkId가 요청 되어서 들어온다면 Translation 엔티티를 생성해 북마크 엔티티의 Translation 값에 추가해준다.
    - 북마크 요청이 아닐 경우에는 DB에 저장되지 않는다.
    
    * feat/translation-4: 북마크 피드백 반영
    - 환경변수 에러 남
    
    * feat/translation-5: 피드반영 UNIT 테스트 오류 해결
    
    - 참고사항 :
    -  .env 파일 변수명 변경 -> DEEPL_TRANSLATE_API_KEY

---
+++ src/main/java/com/dife/api/controller/SwaggerTranslationController.java
@@ -0,0 +1,27 @@
+package com.dife.api.controller;
+
+import com.dife.api.model.dto.TranslationRequestDto;
+import com.dife.api.model.dto.TranslationResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "Translation API", description = "DEEPL API를 이용한 번역 서비스 API")
+public interface SwaggerTranslationController {
+
+	@Operation(
+			summary = "번역 생성 API",
+			description = "text 값에 번역하고자 하는 내용을 적어줍니다. 번역본 언어는 target_lang에 적어줍니다.")
+	@ApiResponse(
+			responseCode = "200",
+			description = "번역 성공 예시",
+			content = {
+				@Content(
+						mediaType = "application/json",
+						schema = @Schema(implementation = TranslationResponseDto.class))
+			})
+	ResponseEntity<TranslationResponseDto> translate(TranslationRequestDto requestDto);
+}
---
+++ src/main/java/com/dife/api/service/TranslationService.java
@@ -0,0 +1,81 @@
+package com.dife.api.service;
+
+import com.dife.api.exception.BookmarkNotFoundException;
+import com.dife.api.model.*;
+import com.dife.api.model.dto.TranslationRequestDto;
+import com.dife.api.model.dto.TranslationResponseDto;
+import com.dife.api.repository.BookmarkRepository;
+import com.dife.api.repository.TranslationRepository;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class TranslationService {
+
+	private final String deeplApiUrl = "https://api-free.deepl.com/v2/translate";
+	private final BookmarkRepository bookmarkRepository;
+	private final TranslationRepository translationRepository;
+
+	@Value("${translation.api-key}")
+	private String authKey;
+
+	public TranslationResponseDto translate(TranslationRequestDto requestDto) {
+		if (requestDto.getBookmarkId() != null) return translateBookmark(requestDto);
+		return translateBasic(requestDto);
+	}
+
+	public TranslationResponseDto translateBasic(TranslationRequestDto requestDto) {
+		String targetLang = requestDto.getTargetLang();
+
+		String apiUrl = deeplApiUrl + "?target_lang=" + targetLang;
+		RestTemplate restTemplate = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.add("Authorization", "DeepL-Auth-Key " + authKey);
+
+		TranslationResponseDto response =
+				restTemplate.postForObject(
+						apiUrl, createHttpEntity(requestDto, headers), TranslationResponseDto.class);
+		return response;
+	}
+
+	public TranslationResponseDto translateBookmark(TranslationRequestDto requestDto) {
+		Bookmark bookmark =
+				bookmarkRepository
+						.findById(requestDto.getBookmarkId())
+						.orElseThrow(BookmarkNotFoundException::new);
+
+		TranslationResponseDto response = translateBasic(requestDto);
+
+		List<Translation> savedTranslations = new ArrayList<>();
+		for (Translation translation : response.getTranslations()) {
+			Translation savedTranslation = translationRepository.save(translation);
+			savedTranslations.add(savedTranslation);
+		}
+
+		bookmark.setTranslations(savedTranslations);
+		bookmarkRepository.save(bookmark);
+
+		return response;
+	}
+
+	private HttpEntity<TranslationRequestDto> createHttpEntity(
+			TranslationRequestDto request, HttpHeaders headers) {
+		return new HttpEntity<>(request, headers);
+	}
+}
