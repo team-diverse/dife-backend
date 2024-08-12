ddef8f4f157d6b28f6d8b32dd8e3bbb471e3a891
suyeon <stgood@kookmin.ac.kr>
2024-08-12 20:44:23 +0900

    feat/member-leave-2: CASACDE.ALL 설정 & 회원탈퇴를 위한 Repository 설정

--- src/main/java/com/dife/api/repository/LikeCommentRepository.java
+++ src/main/java/com/dife/api/repository/LikeCommentRepository.java
@@ -1,14 +1,17 @@
 package com.dife.api.repository;
 
 import com.dife.api.model.*;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LikeCommentRepository extends JpaRepository<LikeComment, Long> {
+public interface LikeCommentRepository extends JpaRepository<CommentLike, Long> {
 
 	Optional<CommentLike> findByCommentAndMember(Comment comment, Member member);
 
 	boolean existsByCommentAndMember(Comment comment, Member member);
+
+	List<CommentLike> findAllByMember(Member member);
 }
--- src/main/java/com/dife/api/repository/TranslationRepository.java
+++ src/main/java/com/dife/api/repository/TranslationRepository.java
@@ -1,8 +1,13 @@
 package com.dife.api.repository;
 
+import com.dife.api.model.Bookmark;
 import com.dife.api.model.Translation;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TranslationRepository extends JpaRepository<Translation, Long> {}
+public interface TranslationRepository extends JpaRepository<Translation, Long> {
+
+	List<Translation> findAllByBookmarks(List<Bookmark> bookmarks);
+}
