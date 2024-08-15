14610b3c8c494212224348d27cd7653bb0af3e0e
Koo Su Yeon <124496650+KooSuYeon@users.noreply.github.com>
2024-08-15 22:33:21 +0900

    fix/post-comments-endpoint-1: 게시글 댓글 조회 엔드포인트 수정 & 댓글 삭제 구현 (#196)
    * fix/post-comments-endpoint-1: 게시글 댓글 조회 엔드포인트 수정 & 댓글 삭제 구현
    
     - 댓글 조회 엔드포인트 :
     - GET: /api/posts/<post_id>/comments
     - 댓글 삭제 엔드포인트 :
     - DELETE : /api/comments/{id}
    
    * fix/post-comments-endpoint-2: 댓글조회는 CommentController에서

--- src/main/java/com/dife/api/controller/PostController.java
+++ src/main/java/com/dife/api/controller/PostController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpStatus.OK;
 
 import com.dife.api.model.BoardCategory;
 import com.dife.api.model.dto.*;
+import com.dife.api.service.CommentService;
 import com.dife.api.service.PostService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,18 +16,19 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/posts")
+@RequestMapping("/api")
 public class PostController implements SwaggerPostController {
 
 	private final PostService postService;
+	private final CommentService commentService;
 
-	@GetMapping
+	@GetMapping("/posts")
 	public ResponseEntity<List<PostResponseDto>> getPostsByBoardType(BoardCategory boardCategory) {
 		List<PostResponseDto> responseDto = postService.getPostsByBoardType(boardCategory);
 		return ResponseEntity.status(OK).body(responseDto);
 	}
 
-	@PostMapping(consumes = "multipart/form-data")
+	@PostMapping(value = "/posts", consumes = "multipart/form-data")
 	public ResponseEntity<PostResponseDto> createPost(
 			@RequestParam(name = "title") String title,
 			@RequestParam(name = "content") String content,
@@ -41,14 +43,14 @@ public class PostController implements SwaggerPostController {
 		return ResponseEntity.status(CREATED).body(responseDto);
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping("/posts/{id}")
 	public ResponseEntity<PostResponseDto> getPost(
 			@PathVariable(name = "id") Long id, Authentication auth) {
 		PostResponseDto responseDto = postService.getPost(id, auth.getName());
 		return ResponseEntity.status(OK).body(responseDto);
 	}
 
-	@PutMapping(value = "/{id}", consumes = "multipart/form-data")
+	@PutMapping(value = "/posts/{id}", consumes = "multipart/form-data")
 	public ResponseEntity<PostResponseDto> updatePost(
 			@PathVariable(name = "id") Long id,
 			@RequestParam(name = "title", required = false) String title,
@@ -62,7 +64,7 @@ public class PostController implements SwaggerPostController {
 		return ResponseEntity.status(OK).body(responseDto);
 	}
 
-	@DeleteMapping("/{id}")
+	@DeleteMapping("/posts/{id}")
 	public ResponseEntity<Void> deletePost(@PathVariable(name = "id") Long id, Authentication auth) {
 		postService.deletePost(id, auth.getName());
 		return new ResponseEntity<>(OK);
