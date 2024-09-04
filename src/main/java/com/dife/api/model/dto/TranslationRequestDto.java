5c5ce2557265bf6eb7a23eacbfc3373dcf0b9c5b
suyeon <stgood@kookmin.ac.kr>
2024-09-04 20:02:03 +0900

    feat/translate-community-5: 제한이 존재하는 번역진행
    - 게시글, 채팅 번역 추가
    - 게시글은 title, content가 그 범위임
    - 회원당 번역 제한 15개

--- src/main/java/com/dife/api/model/dto/TranslationRequestDto.java
+++ src/main/java/com/dife/api/model/dto/TranslationRequestDto.java
@@ -13,6 +13,8 @@ import lombok.Setter;
 public class TranslationRequestDto {
 
 	private List<String> text;
-	private String targetLang;
+	private String target_lang;
+	private Long postId;
+	private Long chatId;
 	private Long bookmarkId;
 }
--- src/main/java/com/dife/api/service/TranslationService.java
+++ src/main/java/com/dife/api/service/TranslationService.java
@@ -1,12 +1,12 @@
 package com.dife.api.service;
 
-import com.dife.api.exception.BookmarkNotFoundException;
+import com.dife.api.exception.*;
 import com.dife.api.model.*;
 import com.dife.api.model.dto.TranslationRequestDto;
 import com.dife.api.model.dto.TranslationResponseDto;
-import com.dife.api.repository.BookmarkRepository;
-import com.dife.api.repository.TranslationRepository;
+import com.dife.api.repository.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,19 +26,29 @@ import org.springframework.web.client.RestTemplate;
 public class TranslationService {
 
 	private final String deeplApiUrl = "https://api-free.deepl.com/v2/translate";
+	private static final Integer RESTRICTED_TRANSLATION_COUNT = 15;
 	private final BookmarkRepository bookmarkRepository;
 	private final TranslationRepository translationRepository;
+	private final PostRepository postRepository;
+	private final MemberRepository memberRepository;
+	private final ChatRepository chatRepository;
 
 	@Value("${translation.api-key}")
 	private String authKey;
 
-	public TranslationResponseDto translate(TranslationRequestDto requestDto) {
-		if (requestDto.getBookmarkId() != null) return translateBookmark(requestDto);
+	public TranslationResponseDto translate(TranslationRequestDto requestDto, String memberEmail) {
+
+		log.info(String.valueOf(requestDto.getPostId()));
+		Member member =
+				memberRepository.findByEmail(memberEmail).orElseThrow(MemberNotFoundException::new);
+		if (requestDto.getBookmarkId() != null) return translateBookmark(requestDto, member);
+		if (requestDto.getPostId() != null) return translatePost(requestDto, member);
+		if (requestDto.getChatId() != null) return translateChat(requestDto, member);
 		return translateBasic(requestDto);
 	}
 
 	public TranslationResponseDto translateBasic(TranslationRequestDto requestDto) {
-		String targetLang = requestDto.getTargetLang();
+		String targetLang = requestDto.getTarget_lang();
 
 		String apiUrl = deeplApiUrl + "?target_lang=" + targetLang;
 		RestTemplate restTemplate = new RestTemplate();
@@ -54,12 +64,17 @@ public class TranslationService {
 		return response;
 	}
 
-	public TranslationResponseDto translateBookmark(TranslationRequestDto requestDto) {
+	public TranslationResponseDto translateBookmark(TranslationRequestDto requestDto, Member member) {
 		Bookmark bookmark =
 				bookmarkRepository
 						.findById(requestDto.getBookmarkId())
 						.orElseThrow(BookmarkNotFoundException::new);
 
+		if (member.getTranslationCount() > RESTRICTED_TRANSLATION_COUNT)
+			throw new TranslationFullException();
+
+		requestDto.setText(Collections.singletonList(bookmark.getMessage()));
+		requestDto.setTarget_lang(member.getSettingLanguage());
 		TranslationResponseDto response = translateBasic(requestDto);
 
 		List<Translation> savedTranslations = new ArrayList<>();
@@ -71,6 +86,47 @@ public class TranslationService {
 		bookmark.setTranslations(savedTranslations);
 		bookmarkRepository.save(bookmark);
 
+		member.setTranslationCount(member.getTranslationCount() + 1);
+		return response;
+	}
+
+	public TranslationResponseDto translatePost(TranslationRequestDto requestDto, Member member) {
+		Post post =
+				postRepository.findById(requestDto.getPostId()).orElseThrow(PostNotFoundException::new);
+
+		if (member.getTranslationCount() > RESTRICTED_TRANSLATION_COUNT)
+			throw new TranslationFullException();
+
+		requestDto.setText(Arrays.asList(post.getTitle(), post.getContent()));
+		requestDto.setTarget_lang(member.getSettingLanguage());
+		return createTranslationResponse(requestDto, member);
+	}
+
+	public TranslationResponseDto translateChat(TranslationRequestDto requestDto, Member member) {
+		Chat chat =
+				chatRepository.findById(requestDto.getChatId()).orElseThrow(ChatNotFoundException::new);
+
+		if (member.getTranslationCount() > RESTRICTED_TRANSLATION_COUNT)
+			throw new TranslationFullException();
+
+		requestDto.setText(Collections.singletonList(chat.getMessage()));
+		requestDto.setTarget_lang(member.getSettingLanguage());
+		return createTranslationResponse(requestDto, member);
+	}
+
+	private TranslationResponseDto createTranslationResponse(
+			TranslationRequestDto requestDto, Member member) {
+		TranslationResponseDto response = translateBasic(requestDto);
+
+		List<Translation> savedTranslations = new ArrayList<>();
+
+		for (Translation translation : response.getTranslations()) {
+			Translation savedTranslation = translationRepository.save(translation);
+			savedTranslations.add(savedTranslation);
+		}
+
+		member.setTranslationCount(member.getTranslationCount() + 1);
+		response.setTranslations(savedTranslations);
 		return response;
 	}
 
