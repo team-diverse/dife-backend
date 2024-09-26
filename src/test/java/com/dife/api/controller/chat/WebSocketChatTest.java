2c1a29988c6b6166a3486cf05464ed815cc06d09
suyeon <stgood@kookmin.ac.kr>
2024-09-26 15:26:41 +0900

    test/chat-stomp-4: 웹소켓 채팅 보내기 테스트 코드 작성
    - 확장적으로 웹소켓 채팅 테스트 코드를 작성했음
    
    - 앞선 ~2 커핏까지 중복된 부분 (모듈화) :
    - 1. 가상의 WebStompClient를 만드는 코드인 getStompClient 메서드로 모듈화
    
    - 2. testConnectWebSocket() :
    - WebSocket에 세션이 2초 안에 CONNECT되는지 확인 된 후 연결된 SESSION의 ID를 확인할 수 있음
    
    - 3. testSubscribeWebSocket():
    - WebSocket이 연결된 후 채팅을 받기 위해 채팅방 구독을 진행하는 메서드임
    - 싱글 채팅방으로 생성된 유저 두 명이 각자의 세션으로 웹소켓 구독을 진행하기까지의 시나리오임
    - 가장 먼저 STOMP CONNECT를 진행하는데 ChatService의 sendMessage에서는 sessionAccessor과 옳지 않은 접근의 경우 Session 연결을 끊어버려야 하기 때문에 StompHeaders를 StompSession을 생성할 때 연결해 기억해둠
    - 그 후 채팅을 받기 위한 채팅방 구독을 진행함
    - * STOMP session 연결 Timelimit은 60s
    - * 채팅방 헤더 정보를 갖고 올 때의 Timelimit 은 10s
    
    - 4. testChatWebSocketAndGetChatroomChats() :
     - 구독을 진행한 채팅방으로 ChatService의 sendMessage() 메서드를 테스트
     - 해당 메서드를 실행한 후 저장되는 chats들을 ChatroomService의 getChats() 메서드로 테스트
      해보는 메서드임
     - 채팅을 보내기 위한 header 사전 설정 :
     - 1. 연결된 StompSession을 id 연결해줌
     - 2. Destination 설정

---
+++ src/test/java/com/dife/api/controller/chat/WebSocketChatTest.java
@@ -0,0 +1,204 @@
+package com.dife.api.controller.chat;
+
+import static org.junit.Assert.*;
+
+import com.dife.api.config.LocalRedisConfig;
+import com.dife.api.config.TestConfig;
+import com.dife.api.model.*;
+import com.dife.api.model.dto.ChatRequestDto;
+import com.dife.api.model.dto.ChatResponseDto;
+import com.dife.api.repository.ChatroomRepository;
+import com.dife.api.service.ChatService;
+import com.dife.api.service.ChatroomService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestConfig.class)
+@ExtendWith(LocalRedisConfig.class)
+@Testcontainers
+@Transactional
+@ActiveProfiles("test")
+public class WebSocketChatTest {
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+	private ObjectMapper objectMapper = new ObjectMapper();
+	@LocalServerPort private int port;
+	private String url;
+
+	private Member member1;
+	private Member member2;
+	private Chatroom chatroom;
+
+	@Autowired private ChatService chatService;
+
+	@Autowired private ChatroomService chatroomService;
+
+	@Autowired private ChatroomRepository chatroomRepository;
+
+	@BeforeEach
+	public void setUp() {
+		url = "ws://localhost:" + port + "/ws";
+		chatroom = chatroomRepository.getReferenceById(1L);
+	}
+
+	public WebSocketStompClient getStompClient() {
+
+		WebSocketClient webSocketClient = new StandardWebSocketClient();
+		WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+		return stompClient;
+	}
+
+	@Test
+	void testConnectWebSocket() {
+
+		Assertions.assertDoesNotThrow(
+				() ->
+						getStompClient()
+								.connect(url, new StompSessionHandlerAdapter() {})
+								.get(2, TimeUnit.SECONDS));
+	}
+
+	@Test
+	void testSubscribeWebSocket() throws Exception {
+
+		CompletableFuture<StompHeaders> completableFuture1 = new CompletableFuture<>();
+		StompSession stompSession1 =
+				getStompClient()
+						.connect(
+								url,
+								new StompSessionHandlerAdapter() {
+									@Override
+									public void handleFrame(StompHeaders headers, Object payload) {
+										completableFuture1.complete(headers);
+									}
+								})
+						.get(2, TimeUnit.SECONDS);
+
+		CompletableFuture<StompHeaders> completableFuture2 = new CompletableFuture<>();
+
+		StompSession stompSession2 =
+				getStompClient()
+						.connect(
+								url,
+								new StompSessionHandlerAdapter() {
+									@Override
+									public void handleFrame(StompHeaders headers, Object payload) {
+										completableFuture2.complete(headers);
+									}
+								})
+						.get(2, TimeUnit.SECONDS);
+		stompSession1.subscribe(
+				"/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
+		stompSession2.subscribe(
+				"/sub/chatroom/" + +chatroom.getId(), new StompSessionHandlerAdapter() {});
+
+		Assertions.assertThrows(
+				TimeoutException.class, () -> completableFuture1.get(10, TimeUnit.SECONDS));
+		Assertions.assertThrows(
+				TimeoutException.class, () -> completableFuture2.get(10, TimeUnit.SECONDS));
+	}
+
+	@Test
+	void testChatWebSocketAndGetChatroomChats() throws Exception {
+
+		Set<Member> memberSet = chatroom.getMembers();
+		List<Member> memberList = new ArrayList<>(memberSet);
+
+		member1 = memberList.get(0);
+		member2 = memberList.get(1);
+
+		CompletableFuture<StompHeaders> completableFuture1 = new CompletableFuture<>();
+		StompSession stompSession1 =
+				getStompClient()
+						.connect(
+								url,
+								new StompSessionHandlerAdapter() {
+
+									@Override
+									public void handleFrame(StompHeaders headers, Object payload) {
+										completableFuture1.complete(headers);
+									}
+								})
+						.get(60, TimeUnit.SECONDS);
+
+		CompletableFuture<StompHeaders> completableFuture2 = new CompletableFuture<>();
+
+		StompSession stompSession2 =
+				getStompClient()
+						.connect(
+								url,
+								new StompSessionHandlerAdapter() {
+
+									@Override
+									public void handleFrame(StompHeaders headers, Object payload) {
+										completableFuture2.complete(headers);
+									}
+								})
+						.get(60, TimeUnit.SECONDS);
+		stompSession1.subscribe("/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
+		stompSession2.subscribe("/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
+
+		for (int i = 0; i < 100; i++) {
+			ChatRequestDto chatMessage = createChatDto(chatroom, member1);
+			SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create();
+			headerAccessor.setSessionId(stompSession1.getSessionId());
+			headerAccessor.setDestination("/pub/chatroom/chat");
+
+			try {
+				chatService.sendMessage(chatMessage, headerAccessor);
+			} catch (JsonProcessingException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		List<ChatResponseDto> chats = chatroomService.getChats(chatroom.getId(), member2.getEmail());
+
+		for (ChatResponseDto chat : chats) {
+			String jsonChat = objectMapper.writeValueAsString(chat);
+			System.out.println("===================");
+			System.out.println("SAVED CHAT: " + jsonChat);
+		}
+
+		Assertions.assertThrows(
+				TimeoutException.class, () -> completableFuture1.get(10, TimeUnit.SECONDS));
+		Assertions.assertThrows(
+				TimeoutException.class, () -> completableFuture2.get(10, TimeUnit.SECONDS));
+	}
+
+	private ChatRequestDto createChatDto(Chatroom chatroom, Member member) {
+		ChatRequestDto requestDto = new ChatRequestDto();
+		requestDto.setChatType(ChatType.CHAT);
+		requestDto.setChatroomId(chatroom.getId());
+		requestDto.setMemberId(member.getId());
+		requestDto.setMessage("채팅 테스트를 해볼게");
+
+		return requestDto;
+	}
+}
