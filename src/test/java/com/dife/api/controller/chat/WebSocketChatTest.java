be8d714ec69567ee79bcd6bbd28a2cdb2bc977d1
suyeon <stgood@kookmin.ac.kr>
2024-09-27 21:46:18 +0900

    test/chat-stomp-7: authorization 에 JWT 주입 테스트 코드 작성
    - 사전 작업 (SetUp) / 공통 사항 :
    - JWT 생성 후 StompHeader에 넣기
    - Client 측에 해당 Session에 Header 적용해 수정
    
    - 웹소켓 연결 / 구독 / 채팅 전송 Authorization / 채팅 전송 테스트 통과
    - 채팅 전송 Authorization 테스트 :
    - CONNECT된 Authorization이 SEND까지 연결되는지 확인하기 위한 목적
    - 연결되지 않는다면 Exception을 일으켜야 함 -> 일으키지 않음 확인
    
    - 채팅 전송 테스트 :
    - 실제 코드에서는 Session이 유지되기 때문에 이미 Authorization이 검증된 유저를 SimpMessageHeaderAccessor에 주입해 ChatService의 sendMessage를 테스트함

--- src/test/java/com/dife/api/controller/chat/WebSocketChatTest.java
+++ src/test/java/com/dife/api/controller/chat/WebSocketChatTest.java
@@ -7,14 +7,15 @@ import com.dife.api.config.TestConfig;
 import com.dife.api.model.*;
 import com.dife.api.model.dto.ChatRequestDto;
 import com.dife.api.model.dto.ChatResponseDto;
+import com.dife.api.model.dto.LoginDto;
+import com.dife.api.model.dto.LoginSuccessDto;
 import com.dife.api.repository.ChatroomRepository;
 import com.dife.api.service.ChatService;
 import com.dife.api.service.ChatroomService;
+import com.dife.api.service.MemberService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,11 +28,10 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.messaging.simp.stomp.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
@@ -55,7 +55,14 @@ public class WebSocketChatTest {
 	private Member member2;
 	private Chatroom chatroom;
 
+	private String testMember1Token;
+	private String testMember2Token;
+
+	private StompHeaders testMember1Header;
+	private StompHeaders testMember2Header;
+
 	@Autowired private ChatService chatService;
+	@Autowired private MemberService memberService;
 
 	@Autowired private ChatroomService chatroomService;
 
@@ -65,6 +72,18 @@ public class WebSocketChatTest {
 	public void setUp() {
 		url = "ws://localhost:" + port + "/ws";
 		chatroom = chatroomRepository.getReferenceById(1L);
+
+		Set<Member> memberSet = chatroom.getMembers();
+		List<Member> memberList = new ArrayList<>(memberSet);
+
+		member1 = memberList.get(0);
+		member2 = memberList.get(1);
+
+		testMember1Token = createTestToken(member1);
+		testMember2Token = createTestToken(member2);
+
+		testMember1Header = createStompHeaders(testMember1Token);
+		testMember2Header = createStompHeaders(testMember2Token);
 	}
 
 	public WebSocketStompClient getStompClient() {
@@ -79,89 +98,90 @@ public class WebSocketChatTest {
 	@Test
 	void testConnectWebSocket() {
 
+		WebSocketHttpHeaders webSocketHttpHeaders = new WebSocketHttpHeaders();
+
 		Assertions.assertDoesNotThrow(
 				() ->
 						getStompClient()
-								.connect(url, new StompSessionHandlerAdapter() {})
+								.connect(
+										url,
+										webSocketHttpHeaders,
+										testMember1Header,
+										new StompSessionHandlerAdapter() {})
 								.get(2, TimeUnit.SECONDS));
 	}
 
 	@Test
 	void testSubscribeWebSocket() throws Exception {
 
-		CompletableFuture<StompHeaders> completableFuture1 = new CompletableFuture<>();
+		WebSocketHttpHeaders webSocketHttpHeaders1 = new WebSocketHttpHeaders();
+
 		StompSession stompSession1 =
 				getStompClient()
 						.connect(
-								url,
-								new StompSessionHandlerAdapter() {
-									@Override
-									public void handleFrame(StompHeaders headers, Object payload) {
-										completableFuture1.complete(headers);
-									}
-								})
+								url, webSocketHttpHeaders1, testMember1Header, new StompSessionHandlerAdapter() {})
 						.get(2, TimeUnit.SECONDS);
 
-		CompletableFuture<StompHeaders> completableFuture2 = new CompletableFuture<>();
+		WebSocketHttpHeaders webSocketHttpHeaders2 = new WebSocketHttpHeaders();
 
 		StompSession stompSession2 =
 				getStompClient()
 						.connect(
-								url,
-								new StompSessionHandlerAdapter() {
-									@Override
-									public void handleFrame(StompHeaders headers, Object payload) {
-										completableFuture2.complete(headers);
-									}
-								})
+								url, webSocketHttpHeaders2, testMember2Header, new StompSessionHandlerAdapter() {})
 						.get(2, TimeUnit.SECONDS);
 		stompSession1.subscribe(
 				"/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
 		stompSession2.subscribe(
-				"/sub/chatroom/" + +chatroom.getId(), new StompSessionHandlerAdapter() {});
+				"/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
+	}
 
-		Assertions.assertThrows(
-				TimeoutException.class, () -> completableFuture1.get(10, TimeUnit.SECONDS));
-		Assertions.assertThrows(
-				TimeoutException.class, () -> completableFuture2.get(10, TimeUnit.SECONDS));
+	@Test
+	void testChatWebSocketAuthorization() throws Exception {
+
+		WebSocketHttpHeaders webSocketHttpHeaders1 = new WebSocketHttpHeaders();
+
+		StompSession stompSession1 =
+				getStompClient()
+						.connect(
+								url, webSocketHttpHeaders1, testMember1Header, new StompSessionHandlerAdapter() {})
+						.get(60, TimeUnit.SECONDS);
+
+		WebSocketHttpHeaders webSocketHttpHeaders2 = new WebSocketHttpHeaders();
+
+		StompSession stompSession2 =
+				getStompClient()
+						.connect(
+								url, webSocketHttpHeaders2, testMember2Header, new StompSessionHandlerAdapter() {})
+						.get(60, TimeUnit.SECONDS);
+		stompSession1.subscribe(
+				"/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
+
+		stompSession2.subscribe(
+				"/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String messageAsJson = objectMapper.writeValueAsString(createChatDto(chatroom));
+
+		testMember1Header.add("destination", "/pub/chatroom/chat");
+		stompSession1.send(testMember1Header, messageAsJson);
 	}
 
 	@Test
 	void testChatWebSocketAndGetChatroomChats() throws Exception {
 
-		Set<Member> memberSet = chatroom.getMembers();
-		List<Member> memberList = new ArrayList<>(memberSet);
+		WebSocketHttpHeaders webSocketHttpHeaders1 = new WebSocketHttpHeaders();
 
-		member1 = memberList.get(0);
-		member2 = memberList.get(1);
-
-		CompletableFuture<StompHeaders> completableFuture1 = new CompletableFuture<>();
 		StompSession stompSession1 =
 				getStompClient()
 						.connect(
-								url,
-								new StompSessionHandlerAdapter() {
-
-									@Override
-									public void handleFrame(StompHeaders headers, Object payload) {
-										completableFuture1.complete(headers);
-									}
-								})
+								url, webSocketHttpHeaders1, testMember1Header, new StompSessionHandlerAdapter() {})
 						.get(60, TimeUnit.SECONDS);
-
-		CompletableFuture<StompHeaders> completableFuture2 = new CompletableFuture<>();
+		WebSocketHttpHeaders webSocketHttpHeaders2 = new WebSocketHttpHeaders();
 
 		StompSession stompSession2 =
 				getStompClient()
 						.connect(
-								url,
-								new StompSessionHandlerAdapter() {
-
-									@Override
-									public void handleFrame(StompHeaders headers, Object payload) {
-										completableFuture2.complete(headers);
-									}
-								})
+								url, webSocketHttpHeaders2, testMember2Header, new StompSessionHandlerAdapter() {})
 						.get(60, TimeUnit.SECONDS);
 		stompSession1.subscribe(
 				"/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
@@ -169,9 +189,10 @@ public class WebSocketChatTest {
 				"/sub/chatroom/" + chatroom.getId(), new StompSessionHandlerAdapter() {});
 
 		for (int i = 0; i < 100; i++) {
-			ChatRequestDto chatMessage = createChatDto(chatroom, member1);
+			ChatRequestDto chatMessage = createChatDto(chatroom);
 			SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create();
 			headerAccessor.setSessionId(stompSession1.getSessionId());
+			headerAccessor.setNativeHeader("memberEmail", member1.getEmail());
 			headerAccessor.setDestination("/pub/chatroom/chat");
 
 			try {
@@ -187,20 +208,30 @@ public class WebSocketChatTest {
 			System.out.println("===================");
 			System.out.println("SAVED CHAT: " + jsonChat);
 		}
-
-		Assertions.assertThrows(
-				TimeoutException.class, () -> completableFuture1.get(10, TimeUnit.SECONDS));
-		Assertions.assertThrows(
-				TimeoutException.class, () -> completableFuture2.get(10, TimeUnit.SECONDS));
 	}
 
-	private ChatRequestDto createChatDto(Chatroom chatroom, Member member) {
+	private ChatRequestDto createChatDto(Chatroom chatroom) {
 		ChatRequestDto requestDto = new ChatRequestDto();
 		requestDto.setChatType(ChatType.CHAT);
 		requestDto.setChatroomId(chatroom.getId());
-		requestDto.setMemberId(member.getId());
 		requestDto.setMessage("채팅 테스트를 해볼게");
 
 		return requestDto;
+	}
+
+	private String createTestToken(Member member) {
+		LoginDto loginDto = new LoginDto();
+		loginDto.setEmail(member.getEmail());
+		loginDto.setPassword("password");
+
+		LoginSuccessDto loginSuccessDto = memberService.login(loginDto).getBody();
+		return loginSuccessDto.getAccessToken();
+	}
+
+	private StompHeaders createStompHeaders(String testToken) {
+		StompHeaders memberHeaders = new StompHeaders();
+		memberHeaders.add("authorization", "Bearer " + testToken);
+
+		return memberHeaders;
 	}
 }
